f728b87776f33a9017b65fc9d0da938be7cceed1
Jan Schaffranek <jan.schaffranek@rub.de>
2018-08-22 18:11:36 +0200

    Updae

--- Day 3/solution_day3.py
+++ Day 3/solution_day3.py
@@ -11,7 +11,8 @@ from load_gtsrb import *
 ################
 
 # Load dataset
-dataset_path = "C:/Users/schaf/Documents/GTSRB/Final_Training/Images/"
+#dataset_path = "C:/Users/schaf/Documents/GTSRB/Final_Training/Images/"
+dataset_path = "C:/Users/jan/Documents/GTSRB/Final_Training/Images/"
 classes = range(43)
 
 ################
@@ -22,12 +23,10 @@ import tensorflow as tf
 
 # Helper function to split the dataset
 def get_train_valid_test(x, y):
-    test_size = 0.3
-    valid_size = 0.2 / (1 - test_size)
     x_train, x_test, y_train, y_test = train_test_split(
-        x, y, test_size=test_size, random_state=42)
+        x, y, test_size=0.3, random_state=42)
     x_train, x_valid, y_train, y_valid = train_test_split(
-        x_train, y_train, test_size=valid_size, random_state=42)
+        x_train, y_train, test_size=0.3, random_state=42)
     return (x_train, y_train), (x_valid, y_valid), (x_test, y_test)
 
 def save_dataset():
@@ -70,7 +69,7 @@ save_dataset()
 
 
 ################
-##     dNN    ##
+##    DNN     ##
 ################
 
 ################
@@ -78,89 +77,91 @@ save_dataset()
 ################
 from layers import *
 
-# Helper function to get minibatches
+# Helper function to get minibatches by random sampling
 def next_batch(x, y, batch_size):
     idx = np.random.randint(0, x.shape[0], batch_size)
     x_batch = x[idx]
     y_batch = y[idx]
     return x_batch, y_batch
 
-# # Model variables
-# num_classes = y_train.shape[1]
-# num_features = x_train.shape[1]
-# train_size, valid_size, test_size = x_train.shape[0], x_valid.shape[0], x_test.shape[0]
-# epochs = 25
-# batch_size = 16
-# learning_rate = 5e-5
-# train_mini_batches = train_size // batch_size
-# valid_mini_batches = valid_size // batch_size
-# test_mini_batches = test_size // batch_size
+# Model variables
+num_classes = y_train.shape[1]
+num_features = x_train.shape[1]
+train_size, valid_size, test_size = x_train.shape[0], x_valid.shape[0], x_test.shape[0]
+epochs = 25
+batch_size = 16
+learning_rate = 5e-5
+train_mini_batches = train_size // batch_size
+valid_mini_batches = valid_size // batch_size
+test_mini_batches = test_size // batch_size
 
-# # Input and Output of the NN
-# x = tf.placeholder(dtype=tf.float32, shape=[None, num_features])
-# y = tf.placeholder(dtype=tf.float32, shape=[None, num_classes])
+dnn_graph = tf.Graph()
+with dnn_graph.as_default():
+    # Input and Output of the NN
+    x = tf.placeholder(dtype=tf.float32, shape=[None, num_features])
+    y = tf.placeholder(dtype=tf.float32, shape=[None, num_classes])
 
-# # Model definition
-# def model(x):
-#     x = fc_layer(x, num_features, 1024, name="fc1")
-#     x = tf.nn.relu(x)
-#     x = fc_layer(x, 1024, 512, name="fc2")
-#     x = tf.nn.relu(x)
-#     x = fc_layer(x, 512, 256, name="fc3")
-#     x = tf.nn.relu(x)
-#     x = fc_layer(x, 256, num_classes, name="fc4")
-#     x = tf.nn.softmax(x)
-#     return x
+    # Model definition
+    def model(x):
+        x = fc_layer(x, num_features, 1024, name="fc1")
+        x = tf.nn.relu(x)
+        x = fc_layer(x, 1024, 512, name="fc2")
+        x = tf.nn.relu(x)
+        x = fc_layer(x, 512, 256, name="fc3")
+        x = tf.nn.relu(x)
+        x = fc_layer(x, 256, num_classes, name="fc4")
+        x = tf.nn.softmax(x)
+        return x
 
-# # TensorFlow Ops to train/test
-# pred_op = model(x)
-# loss_op = tf.losses.softmax_cross_entropy(onehot_labels=y, logits=pred_op)
-# correct_result_op = tf.equal(tf.argmax(pred_op, axis=1), tf.argmax(y, axis=1))
-# accuracy_op = tf.reduce_mean(tf.cast(correct_result_op , tf.float32))
-# optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-# train_op = optimizer.minimize(loss_op)
+    # TensorFlow Ops to train/test
+    pred_op = model(x)
+    loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=pred_op, labels=y))
+    correct_result_op = tf.equal(tf.argmax(pred_op, axis=1), tf.argmax(y, axis=1))
+    accuracy_op = tf.reduce_mean(tf.cast(correct_result_op , tf.float32))
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    train_op = optimizer.minimize(loss_op)
 
-# # Start Training and Testing
-# with tf.Session() as sess:
-#     sess.run(tf.global_variables_initializer())
-#     # Training
-#     print("\n\n Start training!")
-#     for epoch in range(epochs):
-#         train_acc, valid_acc = 0.0, 0.0
-#         train_loss, valid_loss = 0.0, 0.0
-#         # Train the weights
-#         for i in range(train_mini_batches):
-#             epoch_x, epoch_y = next_batch(x_train, y_train, batch_size)
-#             _, c = sess.run([train_op, loss_op], feed_dict={x: epoch_x, y: epoch_y})
-#         # Check the performance of the train set
-#         for i in range(train_mini_batches):
-#             epoch_x, epoch_y = next_batch(x_train, y_train, batch_size)
-#             a, c = sess.run([accuracy_op, loss_op], feed_dict={x: epoch_x, y: epoch_y})
-#             train_acc += a
-#             train_loss += c
-#         train_acc = train_acc / train_mini_batches
-#         # Check the performance of the valid set
-#         for i in range(valid_mini_batches):
-#             epoch_x, epoch_y = epoch_x, epoch_y = next_batch(x_valid, y_valid, batch_size)
-#             a, c = sess.run([accuracy_op, loss_op], feed_dict={x: epoch_x, y: epoch_y})
-#             valid_acc += a
-#             valid_loss += c
-#         valid_acc = valid_acc / valid_mini_batches
-#         print("Epoch: ", epoch+1, " of ", epochs, "- Train loss: ", round(train_loss, 3), 
-#             " Valid loss: ", round(valid_loss, 3), " Train Acc: ", round(train_acc, 3), 
-#             " Valid Acc: ", round(valid_acc, 3))
-#     # Testing
-#     test_acc = 0.0
-#     test_loss = 0.0
-#     print("\n\nFinal testing!")
-#     for i in range(test_mini_batches):
-#             epoch_x, epoch_y = epoch_x, epoch_y = next_batch(x_test, y_test, batch_size)
-#             a, c = sess.run([accuracy_op, loss_op], feed_dict={x: epoch_x, y: epoch_y})
-#             test_acc += a
-#             test_loss += c
-#     test_acc = test_acc / test_mini_batches
-#     print("Test Accuracy:\t", test_acc)
-#     print("Test Loss:\t", test_loss)
+    # # Start Training and Testing
+    # with tf.Session(graph=dnn_graph) as sess:
+    #     sess.run(tf.global_variables_initializer())
+    #     # Training
+    #     print("\n\n Start training!")
+    #     for epoch in range(epochs):
+    #         train_acc, valid_acc = 0.0, 0.0
+    #         train_loss, valid_loss = 0.0, 0.0
+    #         # Train the weights
+    #         for i in range(train_mini_batches):
+    #             epoch_x, epoch_y = next_batch(x_train, y_train, batch_size)
+    #             _, c = sess.run([train_op, loss_op], feed_dict={x: epoch_x, y: epoch_y})
+    #         # Check the performance of the train set
+    #         for i in range(train_mini_batches):
+    #             epoch_x, epoch_y = next_batch(x_train, y_train, batch_size)
+    #             a, c = sess.run([accuracy_op, loss_op], feed_dict={x: epoch_x, y: epoch_y})
+    #             train_acc += a
+    #             train_loss += c
+    #         train_acc = train_acc / train_mini_batches
+    #         # Check the performance of the valid set
+    #         for i in range(valid_mini_batches):
+    #             epoch_x, epoch_y = epoch_x, epoch_y = next_batch(x_valid, y_valid, batch_size)
+    #             a, c = sess.run([accuracy_op, loss_op], feed_dict={x: epoch_x, y: epoch_y})
+    #             valid_acc += a
+    #             valid_loss += c
+    #         valid_acc = valid_acc / valid_mini_batches
+    #         print("Epoch: ", epoch+1, " of ", epochs, "- Train loss: ", round(train_loss, 3), 
+    #             " Valid loss: ", round(valid_loss, 3), " Train Acc: ", round(train_acc, 3), 
+    #             " Valid Acc: ", round(valid_acc, 3))
+    #     # Testing
+    #     test_acc = 0.0
+    #     test_loss = 0.0
+    #     print("\n\nFinal testing!")
+    #     for i in range(test_mini_batches):
+    #             epoch_x, epoch_y = epoch_x, epoch_y = next_batch(x_test, y_test, batch_size)
+    #             a, c = sess.run([accuracy_op, loss_op], feed_dict={x: epoch_x, y: epoch_y})
+    #             test_acc += a
+    #             test_loss += c
+    #     test_acc = test_acc / test_mini_batches
+    #     print("Test Accuracy:\t", test_acc)
+    #     print("Test Loss:\t", test_loss)
 
 
 
@@ -176,87 +177,88 @@ num_classes = y_train.shape[1]
 num_features = x_train.shape[1]
 train_size, valid_size, test_size = x_train.shape[0], x_valid.shape[0], x_test.shape[0]
 epochs = 50
-batch_size = 16
-learning_rate = 5e-4
+batch_size = 64
 train_mini_batches = train_size // batch_size
 valid_mini_batches = valid_size // batch_size
 test_mini_batches = test_size // batch_size
+learning_rate = 2e-4
 
-# Input and Output of the NN
-x = tf.placeholder(dtype=tf.float32, shape=[None, num_features])
-y = tf.placeholder(dtype=tf.float32, shape=[None, num_classes])
+cnn_graph = tf.Graph()
+with cnn_graph.as_default():
+    # Input and Output of the NN
+    x = tf.placeholder(dtype=tf.float32, shape=[None, num_features])
+    y = tf.placeholder(dtype=tf.float32, shape=[None, num_classes])
 
-# Model definition
-def model(x):
-    x = tf.reshape(x, shape=[-1, 32, 32, 3]) #32x32
-    x = conv_layer(x, 3, 32, k_size=3, name="conv1")
-    x = tf.nn.relu(x)
-    x = conv_layer(x, 32, 32, k_size=3, name="conv11")
-    x = tf.nn.relu(x)
-    x = max_pool(x) #16x16
-    x = conv_layer(x, 32, 64, k_size=3, name="conv2")
-    x = tf.nn.relu(x)
-    x = conv_layer(x, 64, 64, k_size=3, name="conv22")
-    x = tf.nn.relu(x)
-    x = max_pool(x) # 8x8
-    x = conv_layer(x, 64, 64, k_size=3, name="conv3")
-    x = tf.nn.relu(x)
-    x = conv_layer(x, 64, 64, k_size=3, name="conv33")
-    x = tf.nn.relu(x)
-    x = max_pool(x) # 4x4
-    x = flatten(x)
-    x = fc_layer(x, 64*4*4, 300, name="fc1")
-    x = fc_layer(x, 300, 50, name="fc2")
-    x = fc_layer(x, 50, num_classes, name="fc3")
-    x = tf.nn.softmax(x)
-    return x
+    # Model definition
+    def model(x):
+        x = tf.reshape(x, shape=[-1, 32, 32, 3]) #32x32
+        x = conv_layer(x, 3, 32, k_size=3, name="conv1")
+        x = tf.nn.relu(x)
+        x = conv_layer(x, 32, 32, k_size=3, name="conv11")
+        x = tf.nn.relu(x)
+        x = max_pool(x) #16x16
+        x = conv_layer(x, 32, 64, k_size=3, name="conv2")
+        x = tf.nn.relu(x)
+        x = conv_layer(x, 64, 64, k_size=3, name="conv22")
+        x = tf.nn.relu(x)
+        x = max_pool(x) # 8x8
+        x = conv_layer(x, 64, 128, k_size=3, name="conv3")
+        x = tf.nn.relu(x)
+        x = conv_layer(x, 128, 128, k_size=3, name="conv33")
+        x = tf.nn.relu(x)
+        x = max_pool(x) # 4x4
+        x = flatten(x)
+        x = fc_layer(x, 128*4*4, 512, name="fc1")
+        x = fc_layer(x, 512, num_classes, name="fc2")
+        x = tf.nn.softmax(x)
+        return x
 
-# TensorFlow Ops to train/test
-pred_op = model(x)
-loss_op = tf.losses.softmax_cross_entropy(onehot_labels=y, logits=pred_op)
-correct_result_op = tf.equal(tf.argmax(pred_op, axis=1), tf.argmax(y, axis=1))
-accuracy_op = tf.reduce_mean(tf.cast(correct_result_op , tf.float32))
-optimizer = tf.train.RMSPropOptimizer(learning_rate=learning_rate)
-train_op = optimizer.minimize(loss_op)
+    # TensorFlow Ops to train/test
+    pred_op = model(x)
+    loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=pred_op, labels=y))
+    correct_result_op = tf.equal(tf.argmax(pred_op, axis=1), tf.argmax(y, axis=1))
+    accuracy_op = tf.reduce_mean(tf.cast(correct_result_op , tf.float32))
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    train_op = optimizer.minimize(loss_op)
 
-# Start Training and Testing
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    # Training
-    print("\n\n Start training!")
-    for epoch in range(epochs):
-        train_acc, valid_acc = 0.0, 0.0
-        train_loss, valid_loss = 0.0, 0.0
-        # Train the weights
-        for i in range(train_mini_batches):
-            epoch_x, epoch_y = next_batch(x_train, y_train, batch_size)
-            _, c = sess.run([train_op, loss_op], feed_dict={x: epoch_x, y: epoch_y})
-        # Check the performance of the train set
-        for i in range(train_mini_batches):
-            epoch_x, epoch_y = next_batch(x_train, y_train, batch_size)
-            a, c = sess.run([accuracy_op, loss_op], feed_dict={x: epoch_x, y: epoch_y})
-            train_acc += a
-            train_loss += c
-        train_acc = train_acc / train_mini_batches
-        # Check the performance of the valid set
-        for i in range(valid_mini_batches):
-            epoch_x, epoch_y = epoch_x, epoch_y = next_batch(x_valid, y_valid, batch_size)
-            a, c = sess.run([accuracy_op, loss_op], feed_dict={x: epoch_x, y: epoch_y})
-            valid_acc += a
-            valid_loss += c
-        valid_acc = valid_acc / valid_mini_batches
-        print("Epoch: ", epoch+1, " of ", epochs, "- Train loss: ", round(train_loss, 3), 
-            " Valid loss: ", round(valid_loss, 3), " Train Acc: ", round(train_acc, 3), 
-            " Valid Acc: ", round(valid_acc, 3))
-    # Testing
-    test_acc = 0.0
-    test_loss = 0.0
-    print("\n\nFinal testing!")
-    for i in range(test_mini_batches):
-            epoch_x, epoch_y = epoch_x, epoch_y = next_batch(x_test, y_test, batch_size)
-            a, c = sess.run([accuracy_op, loss_op], feed_dict={x: epoch_x, y: epoch_y})
-            test_acc += a
-            test_loss += c
-    test_acc = test_acc / test_mini_batches
-    print("Test Accuracy:\t", test_acc)
-    print("Test Loss:\t", test_loss)
+    # Start Training and Testing
+    with tf.Session(graph=cnn_graph) as sess:
+        sess.run(tf.global_variables_initializer())
+        # Training
+        print("\n\n Start training!")
+        for epoch in range(epochs):
+            train_acc, valid_acc = 0.0, 0.0
+            train_loss, valid_loss = 0.0, 0.0
+            # Train the weights
+            for i in range(train_mini_batches):
+                epoch_x, epoch_y = next_batch(x_train, y_train, batch_size)
+                _, c = sess.run([train_op, loss_op], feed_dict={x: epoch_x, y: epoch_y})
+            # Check the performance of the train set
+            for i in range(train_mini_batches):
+                epoch_x, epoch_y = next_batch(x_train, y_train, batch_size)
+                a, c = sess.run([accuracy_op, loss_op], feed_dict={x: epoch_x, y: epoch_y})
+                train_acc += a
+                train_loss += c
+            train_acc = train_acc / train_mini_batches
+            # Check the performance of the valid set
+            for i in range(valid_mini_batches):
+                epoch_x, epoch_y = epoch_x, epoch_y = next_batch(x_valid, y_valid, batch_size)
+                a, c = sess.run([accuracy_op, loss_op], feed_dict={x: epoch_x, y: epoch_y})
+                valid_acc += a
+                valid_loss += c
+            valid_acc = valid_acc / valid_mini_batches
+            print("Epoch: ", epoch+1, " of ", epochs, "- Train loss: ", round(train_loss, 3), 
+                " Valid loss: ", round(valid_loss, 3), " Train Acc: ", round(train_acc, 3), 
+                " Valid Acc: ", round(valid_acc, 3))
+        # Testing
+        test_acc = 0.0
+        test_loss = 0.0
+        print("\n\nFinal testing!")
+        for i in range(test_mini_batches):
+                epoch_x, epoch_y = epoch_x, epoch_y = next_batch(x_test, y_test, batch_size)
+                a, c = sess.run([accuracy_op, loss_op], feed_dict={x: epoch_x, y: epoch_y})
+                test_acc += a
+                test_loss += c
+        test_acc = test_acc / test_mini_batches
+        print("Test Accuracy:\t", test_acc)
+        print("Test Loss:\t", test_loss)
