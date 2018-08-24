297b98fa5d28ab54a798a80d31a3a93d88272a76
Jan Schaffranek <jan.schaffranek@rub.de>
2018-08-24 19:27:56 +0200

    Added LaTeX

--- Day 4/helper.py
+++ Day 4/helper.py
@@ -17,14 +17,9 @@ def occlusion(img, label, box_size, sess, pred_op, x):
         for j in range(img.shape[1]-box_size+1):
             img_s = img.copy()
             img_s[i:box_size+i, j:box_size+j] = gray_box
-            x_p = sess.run([pred_op], feed_dict={x: img_s.reshape(-1, img_s.shape[0], img_s.shape[1], img_s.shape[2])})
-            print(x_p)
-            print(x_p.shape)
+            x_p = sess.run([pred_op], feed_dict={x: img_s.reshape(-1, img_s.shape[0], img_s.shape[1], img_s.shape[2])})[0]
             x_p = sess.run(tf.nn.softmax(x_p))
-            print(x_p)
-            print(x_p.shape)
             x_p = np.reshape(x_p, (label.shape[0]))
-            #x_p = np.exp(x_p) / np.sum(np.exp(x_p), axis=0)
             prediction_heatmap[i][j] = x_p[np.argmax(label)]
     occlusion_plot(prediction_heatmap)
 
@@ -33,7 +28,7 @@ def display_convergence_error(train_error, valid_error):
     plt.plot(range(1, len(train_error)+1), train_error, color="red")
     plt.plot(range(1, len(valid_error)+1), valid_error, color="blue")
     plt.legend(["Train", "valid"])
-    plt.title('Error of the NN')
+    plt.title('Errors of the NN')
     plt.xlabel('Epoch')
     plt.ylabel('Error')
     plt.show()
@@ -47,3 +42,15 @@ def display_convergence_acc(train_acc, valid_acc):
     plt.xlabel('Epoch')
     plt.ylabel('Acc')
     plt.show()
+
+def get_model_params():
+    gvars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
+    return {gvar.op.name: value for gvar, value in zip(gvars, tf.get_default_session().run(gvars))}
+
+def restore_model_params(model_params):
+    gvar_names = list(model_params.keys())
+    assign_ops = {gvar_name: tf.get_default_graph().get_operation_by_name(gvar_name + "/Assign")
+                  for gvar_name in gvar_names}
+    init_values = {gvar_name: assign_op.inputs[1] for gvar_name, assign_op in assign_ops.items()}
+    feed_dict = {init_values[gvar_name]: model_params[gvar_name] for gvar_name in gvar_names}
+    tf.get_default_session().run(assign_ops, feed_dict=feed_dict)
--- Day 4/solution_day4.py
+++ Day 4/solution_day4.py
@@ -13,15 +13,15 @@ from layers import *
 from helper import *
 
 # Load dataset
-dataset_path = "C:/Users/schaf/Documents/GTSRB/Final_Training/Images/"
-#dataset_path = "C:/Users/jan/Documents/GTSRB/Final_Training/Images/"
+#dataset_path = "C:/Users/schaf/Documents/GTSRB/Final_Training/Images/"
+dataset_path = "C:/Users/jan/Documents/GTSRB/Final_Training/Images/"
 
-num_classes = 2
-imgs_per_class = 100
+num_classes = 43
+imgs_per_class = 10000
 classes = range(num_classes)
 save_dataset(dataset_path, classes, imgs_per_class=imgs_per_class)
 data = GTSRB(dataset_path, num_classes)
-#data.data_augmentation(augment_size=10000)
+data.data_augmentation(augment_size=10000)
 # Model variables
 train_size, valid_size, test_size = data.train_size, data.valid_size, data.test_size
 img_width, img_height, img_depth = data.img_width, data.img_height, data.img_depth
@@ -29,30 +29,30 @@ img_width, img_height, img_depth = data.img_width, data.img_height, data.img_dep
 ################
 ## EXERCISE 1 ##
 ################
-
+ 
 # Hyperparameters
-epochs = 3
+epochs = 50
 batch_size = 32
-learning_rate = 1e-4
-optimizer = tf.train.RMSPropOptimizer
+learning_rate = 1e-3
+optimizer = tf.train.AdamOptimizer
 # Helper variables
 train_mini_batches = train_size // batch_size
 valid_mini_batches = valid_size // batch_size
 test_mini_batches = test_size // batch_size
 # Variables for early stopping in training
 early_stopping_crit = "accuracy"
-early_stopping_patience = 50
+early_stopping_patience = 100
 
 cnn_graph = tf.Graph()
 with cnn_graph.as_default():
     # Input and Output of the NN
     x = tf.placeholder(dtype=tf.float32, shape=[None, img_width, img_height, img_depth])
     y = tf.placeholder(dtype=tf.float32, shape=[None, num_classes])
-    is_training = tf.placeholder_with_default(False, shape=())
+    training = tf.placeholder_with_default(False, shape=())
     keep_prob = tf.placeholder_with_default(1.0, shape=())
 
 # Model definition
-def cnn_model(x, is_training, keep_prob, cnn_graph):
+def cnn_model(x, training, keep_prob, cnn_graph):
     # Aufgabe 1: Das Netzwerk ist hier
     # Aufgabe 2: Xavier Init ist standard für die Conv und Fc Layer, aus layers.py
     # Aufgabe 3: Data Augmentation weiter oben angewendet und kommt aus load_gtsrb.py
@@ -60,58 +60,50 @@ def cnn_model(x, is_training, keep_prob, cnn_graph):
     # Aufgabe 5: Dropout und BatchNorm siehe unten, aus layers.py
     # Aufgabe 6: Early stopping unten verwendet
     with cnn_graph.as_default():
-        x = conv_layer(x, 3, 32, k_size=3, name="conv1", initializer="xavier") # 32x32
+        x = conv_layer(x, filters=32, k_size=3, name="conv1") # 32x32
         x = tf.nn.relu(x)
-        x = dropout(x, 0.3)
-        x = conv_layer(x, 32, 32, k_size=3, name="conv11", initializer="xavier")
+        x = conv_layer(x, filters=64, k_size=3, name="conv2")
         x = tf.nn.relu(x)
-        x = dropout(x, 0.3)
         x = max_pool(x) #16x16
+        x = dropout(x, 0.25, training=training)
 
-        x = conv_layer(x, 32, 64, k_size=3, name="conv2", initializer="xavier")
+        x = conv_layer(x, filters=32, k_size=3, name="conv3")
         x = tf.nn.relu(x)
-        x = dropout(x, 0.3)
-        x = conv_layer(x, 64, 64, k_size=3, name="conv22", initializer="xavier")
+        x = conv_layer(x, filters=32, k_size=5, name="conv4")
         x = tf.nn.relu(x)
-        x = dropout(x, 0.3)
         x = max_pool(x) # 8x8
 
-        x = conv_layer(x, 64, 128, k_size=3, name="conv3", initializer="xavier")
-        x = tf.nn.relu(x)
-        x = dropout(x, 0.3)
-        x = conv_layer(x, 128, 128, k_size=3, name="conv33", initializer="xavier")
-        x = tf.nn.relu(x)
-        x = dropout(x, 0.3)
-        x = max_pool(x) # 4x4
-
         x = flatten(x)
-        x = fc_layer(x, 128*4*4, 1024, name="fc1", initializer="xavier")
+        x = dropout(x, 0.25, training=training)
+
+        x = dense_layer(x, units=128, name="fc1")
         x = tf.nn.relu(x)
-        x = dropout(x, 0.5)
-        x = fc_layer(x, 1024, num_classes, name="fc2", initializer="xavier")
+        x = dropout(x, 0.5, training=training)
+        x = dense_layer(x, units=num_classes, name="fc2")
         return x
 
 with cnn_graph.as_default():
     # TensorFlow Ops to train/test
-    pred_op = cnn_model(x, is_training, keep_prob, cnn_graph)
+    pred_op = cnn_model(x, training, keep_prob, cnn_graph)
     loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=pred_op, labels=y))
     correct_result_op = tf.equal(tf.argmax(pred_op, axis=1), tf.argmax(y, axis=1))
     accuracy_op = tf.reduce_mean(tf.cast(correct_result_op , tf.float32))
     optimizer = optimizer(learning_rate=learning_rate)
-    #update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-    #with tf.control_dependencies(update_ops):
-    train_op = optimizer.minimize(loss_op)
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(update_ops):
+        train_op = optimizer.minimize(loss_op)
 
 # Start Training and Testing
 with tf.Session(graph=cnn_graph) as sess:
     saver = tf.train.Saver()
-    #saver.restore(sess, tf.train.latest_checkpoint(os.path.dirname(os.path.realpath(__file__))))
     sess.run(tf.global_variables_initializer())
     # Training
     print("\n\n Start training!")
     train_accs, valid_accs = [], []
     train_losses, valid_losses = [], []
     last_valid_acc = 0.0
+    best_valid_acc = 0.0
+    best_model_params = None
     for epoch in range(epochs):
         # Variables to track performance
         train_acc, valid_acc = 0.0, 0.0
@@ -120,7 +112,7 @@ with tf.Session(graph=cnn_graph) as sess:
         for i in range(train_mini_batches):
             epoch_x, epoch_y = data.next_train_batch(batch_size)
             _, c = sess.run([train_op, loss_op], 
-                feed_dict={x: epoch_x, y: epoch_y, is_training: True, keep_prob: 0.8})
+                feed_dict={x: epoch_x, y: epoch_y, training: True, keep_prob: 0.8})
         # Check the performance of the train set
         for i in range(train_mini_batches):
             epoch_x, epoch_y = data.next_train_batch(batch_size)
@@ -144,6 +136,8 @@ with tf.Session(graph=cnn_graph) as sess:
         if early_stopping_patience == 0:
             print("Early Stopping!")
             break
+        if valid_acc > best_valid_acc:
+            best_model_params = get_model_params()
         last_valid_acc = valid_acc
         train_losses.append(train_loss)
         valid_losses.append(valid_loss)
@@ -152,6 +146,8 @@ with tf.Session(graph=cnn_graph) as sess:
     display_convergence_error(train_losses, valid_losses)
     display_convergence_acc(train_accs, valid_accs)
     # Testing
+    if best_model_params:
+        restore_model_params(best_model_params)
     test_acc = 0.0
     test_loss = 0.0
     print("\n\nFinal testing!")
@@ -167,4 +163,6 @@ with tf.Session(graph=cnn_graph) as sess:
     img, label = data.x_test[10], data.y_test[10]
     box_size = 5
     occlusion(img, label, box_size, sess, pred_op, x)
-    #saver.save(sess, os.path.dirname(os.path.realpath(__file__)) + '/tsd_model', global_step=epoch_cnt, write_meta_graph=False)
+    # Save model
+    acc_str = str(test_acc)
+    save_path = saver.save(sess, "./saves_cnn/cnn_model_save"+acc_str[2:]+".ckpt")
